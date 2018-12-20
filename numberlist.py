7857e7c67500f7be2b0500bceee5244926e0748c
Tom Jhuang (莊雅驛) <08140@unimicron.com>
2018-12-20 17:55:53 +0800

    插入排序法

--- numberlist.py
+++ numberlist.py
@@ -78,15 +78,75 @@ def 選擇排序法():
         print('%5d' %i,end = '')
     return
 
+
+
 def 插入排序法():
     print('插入排序法')
+##    RandomNumbers()
+
+    data = [101,94,12,95,84,70,100]
+
+    for i in range(1,N):
+        tmp = data[i]
+        no = i-1
+
+        print('tmp =' ,tmp )
+
+        while no >= 0 and tmp < data[no]:
+            print('tmp (', tmp ,')< data[i-1] (' ,data[no], ')')
+            data[no+1] = data[no]
+            no=no-1
+
+            data[no+1] = tmp
+            print(no)
+            print('\n','------------------------')
+            for i in range(N):
+                print('%5d' %data[i],end = '')
+            print('\n','------------------------')
+
+##            print('tmp =' ,tmp )
+##            print('data[',no,']+1 =' ,data[no+1] )
+##            print('data[',no+1,']+2 =' ,data[no+1] )
+
+
+    for i in range(N):
+        print('%5d' %data[i],end = '')
+
+def 插入排序法2():
+    print('插入排序法2')
     RandomNumbers()
+
+    for i in range(1,N-1):
+        tmp = data[i]
+        no = i-1
+##        print(tmp)
+        while i-1 >= 0 and tmp < data[i-1]:
+##            print('data[',i,']', data[i],'<','data[',i-1,']', data[i-1])
+            data[i] = data[i-1]
+            i=i-1
+##            print('no =',i-1)
+            data[i] = tmp
+
+##            print('data[i] = ' ,data[i])
+##            print('data[i-1] = ' ,data[no])
+##
+##            print('data[',i,']', data[i],'<','data[',no,']', data[no])
+
+##        print('\n','------------------------')
+##        for i in range(N):
+##            print('%5d' %data[i],end = '')
+##        print('\n','------------------------')
+
+
+    print('\n','------------------------')
+    for i in range(N):
+        print('%5d' %data[i],end = '')
 
 
 def main():
 ##    氣泡排序法2()
 ##    選擇排序法()
-    插入排序法()
+    插入排序法2()
     pass
 
 if __name__ == '__main__':
